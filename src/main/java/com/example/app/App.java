21b1f122907e6ba1df799bab7b3b95b791342759
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2024-10-26 23:57:41 +0545

    feat: init navigation

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,9 +1,10 @@
 package com.example.app;
 
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.fxml.FXMLLoader;
 import javafx.application.Application;
 
 /**
@@ -12,12 +13,18 @@ import javafx.application.Application;
 public class App extends Application {
 
   @Override
-  public void start(Stage stage) {
-    var javaVersion = SystemInfo.javaVersion();
-    var javafxVersion = SystemInfo.javafxVersion();
+  public void start(Stage stage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/app.fxml"));
 
-    var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-    var scene = new Scene(new StackPane(label), 640, 480);
+    Parent root = loader.load();
+    Scene scene = new Scene(root);
+
+    stage.setMinWidth(1024);
+    stage.setMinHeight(720);
+    stage.setTitle("Sales-Trail");
+
+    stage.getIcons().add(new Image(getClass().getResourceAsStream("images/icon.png")));
+
     stage.setScene(scene);
     stage.show();
   }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.example.app {
   requires javafx.controls;
+  requires javafx.fxml;
 
   exports com.example.app;
 }
